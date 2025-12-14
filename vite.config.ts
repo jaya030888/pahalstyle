bce8b7deb2c6e93eb50f198a0215c0ab7712bfd4
Sanny1921 <79357283+Sanny1921@users.noreply.github.com>
2025-12-14 14:17:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: './',
+    base: '/pahalstyle/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
